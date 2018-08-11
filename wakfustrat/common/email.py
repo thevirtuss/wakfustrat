69b2b2831f31b6753ee5f916a7e736ce030cfddf
thevirtuss <virtuss.le.pire@gmail.com>
2018-08-11 19:45:45 +0200

    v0.9.8

--- wakfustrat/common/email.py
+++ wakfustrat/common/email.py
@@ -16,6 +16,6 @@ def send_email(subject, to, template, context=None):
 
     body = render_to_string(template, default_context)
 
-    msg = EmailMessage('[WakfuStrat] {0}'.format(subject), body, 'no-reply@wakfustrat.fr', to)
+    msg = EmailMessage('[WakfuStrat] {0}'.format(subject), body, 'contact@wakfustrat.fr', to)
     msg.content_subtype = 'html'
     msg.send()
